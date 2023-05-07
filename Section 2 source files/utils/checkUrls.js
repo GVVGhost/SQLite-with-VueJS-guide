a6e3f205f1e6cb8ab1f1d7aead7b557445b566ad
Vadim <33375723+GVVGhost@users.noreply.github.com>
2023-05-07 11:29:44 +0300

    Update checkUrls.js

--- Section 2 source files/utils/checkUrls.js
+++ Section 2 source files/utils/checkUrls.js
@@ -1,18 +1,20 @@
-import {isValidUrl} from '@/utils/UrlValidator'
+function isValidUrl(urlString) {
+    const urlPattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+    return !!urlPattern.test(urlString);
+}
 
-const url1 = ""
-const url2 = "invalidURL"
-const url3 = "htt//jsowl"
-const url4 = "1"
-const url5 = "https://www.jsowl.com/remove-an-item-from-an-array-in-javascript/"
-const url6 = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
-const url7 = "https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399__480.png"
-
-console.log(`Url valid check`)
-console.log(`${isValidUrl(url1)}: ${url1}`)
-console.log(`${isValidUrl(url2)}: ${url2}`)
-console.log(`${isValidUrl(url3)}: ${url3}`)
-console.log(`${isValidUrl(url4)}: ${url4}`)
-console.log(`${isValidUrl(url5)}: ${url5}`)
-console.log(`${isValidUrl(url6)}: ${url6}`)
-console.log(`${isValidUrl(url7)}: ${url7}`)
+console.log(`URL validation:`);
+[
+    "",
+    "invalidURL",
+    "htt//jsowl",
+    "1",
+    "https://www.jsowl.com/remove-an-item-from-an-array-in-javascript/",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+    "https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399__480.png"
+].forEach((el => console.log(`\t${isValidUrl(el)}: ${el}`)));
