4f13d9810801ef6380fc234226987403f46d53a8
Vadim <33375723+GVVGhost@users.noreply.github.com>
2023-05-08 12:58:10 +0300

    Update SimpleComponent.vue

--- Section 2 source files/components/SimpleComponent.vue
+++ Section 2 source files/components/SimpleComponent.vue
@@ -3,36 +3,31 @@
         <b-row>
             <div class="col-6">
                 <b-card>
-                    <b-container fluid>
-                        <b-row class="my-3" align-v="center">
-                            <b-col sm="2">
-                                <label>Поле name:</label>
-                            </b-col>
-                            <b-col sm="10">
-                                <b-form-input v-model="valName" id="name" type="text"></b-form-input>
-                            </b-col>
-                        </b-row>
-                        <b-row class="my-3" align-v="center">
-                            <b-col sm="2">
-                                <label>Поле url:</label>
-                            </b-col>
-                            <b-col sm="10">
-                                <b-form-input v-model="valUrl" id="link" type="url"></b-form-input>
-                            </b-col>
-                        </b-row>
-                    </b-container>
+                    <b-row class="my-3" align-v="center">
+                        <b-col sm="2"><label>Поле name:</label></b-col>
+                        <b-col sm="10">
+                            <b-form-input v-model="valName" type="text"></b-form-input>
+                        </b-col>
+                    </b-row>
+                    <b-row class="my-3" align-v="center">
+                        <b-col sm="2"><label>Поле url:</label></b-col>
+                        <b-col sm="10">
+                            <b-form-input v-model="valUrl" type="url"></b-form-input>
+                        </b-col>
+                    </b-row>
                     <b-row align-v="center">
                         <b-col sm="6">
-                            <b-button class="container-fluid mt-1" @click="insertToDB" :disabled="btnDisabled"
-                                      variant="outline-secondary">
+                            <b-button class="container-fluid"
+                                      @click="insertToDB"
+                                      :disabled="btnDisabled"
+                                      variant="outline-primary">
                                 {{ !dbRewriteEnable ? "Записати в БД" : "Перезаписати значення із БД" }}
                             </b-button>
                         </b-col>
                         <b-col sm="6">
-                            <b-button class="container-fluid mt-1"
+                            <b-button class="container-fluid"
                                       :pressed="dbRewriteEnable"
                                       @click="dbRewriteEnable = !dbRewriteEnable"
-                                      v-model="dbRewriteEnable"
                                       variant="outline-warning">
                                 {{ dbRewriteEnable ? "Режим перезапису" : "Режим запису" }}
                             </b-button>
@@ -40,13 +35,14 @@
                     </b-row>
                     <b-row class="my-3" align-v="center">
                         <b-col sm="4">
-                            <b-button class="container-fluid" id="id" :disabled="(this.valId.trim() && dbRewriteEnable)"
-                                      @click="deleteBy" variant="outline-danger">
+                            <b-button class="container-fluid"
+                                      :disabled="(this.valId.trim() && dbRewriteEnable)"
+                                      @click="deleteFromDB" variant="outline-danger">
                                 {{ !dbRewriteEnable ? "Видалити id:" : "Id для перезапису:" }}
                             </b-button>
                         </b-col>
                         <b-col sm="8">
-                            <b-form-input v-model="valId" id="id" type="text"></b-form-input>
+                            <b-form-input v-model="valId" type="text"></b-form-input>
                         </b-col>
                     </b-row>
                 </b-card>
@@ -54,8 +50,11 @@
             <div class="col-6">
                 <b-card>
                     <p>Вміст бази даних (імітація)</p>
-                    <b-table class="text-decoration-none" sticky-header :items="tableList"
-                             :fields="tableFields" @row-clicked="onRowSelected"></b-table>
+                    <b-table class="text-decoration-none"
+                             sticky-header
+                             :items="tableList"
+                             :fields="tableFields"
+                             @row-clicked="onRowSelected"></b-table>
                     <div v-if="currImageUrl.trim()" class="row-cols-sm-1">
                         <b-img :src='currImageUrl' sizes="sm" alt="image"></b-img>
                     </div>
@@ -74,19 +73,18 @@ export default {
     name: 'SimpleComponent',
     data() {
         return {
+            dbRewriteEnable: false,
             tableList: tableContent,
             tableFields: ['id', 'name'],
             valId: '',
             valName: '',
             valUrl: '',
-            dbRewriteEnable: false,
             currImageUrl: '',
         }
     },
     methods: {
         insertToDB() {
             const index = this.tableList.findIndex(item => item.id === this.valId);
-            console.log(`Index: ${index}`)
             if (this.dbRewriteEnable && index >= 0) {
                 const temp = this.tableList[index]
                 temp.name = this.valName
@@ -100,28 +98,29 @@ export default {
                 });
             }
         },
-        deleteBy(event) {
-            if ('id' === event.srcElement.id) this.tableList = this.tableList.filter((el) => this.valId !== el.id)
+        deleteFromDB() {
+            this.tableList = this.tableList.filter((el) => this.valId !== el.id)
+            this.clearFields()
         },
         onRowSelected(item) {
-            if (item.id === this.valId
-                && item.name === this.valName
-                && item.url === this.valUrl) {
-                this.valId = ''
-                this.valName = ''
-                this.valUrl = ''
-                this.currImageUrl = ''
-                this.dbRewriteEnable = false
+            if (item.id === this.valId && item.name === this.valName && item.url === this.valUrl) {
+                this.clearFields()
             } else {
                 console.log(item)
                 this.valId = item.id
                 this.valName = item.name
                 this.valUrl = item.url
                 this.currImageUrl = isValidUrl(item.url) ? item.url : ''
-                this.dbRewriteEnable = true
             }
+        },
+        clearFields() {
+            this.valId = ''
+            this.valName = ''
+            this.valUrl = ''
+            this.currImageUrl = ''
         }
-    }, computed: {
+    },
+    computed: {
         btnDisabled() {
             return !(this.valName.trim() && this.valUrl.trim()) && (!this.valName.trim())
         }
